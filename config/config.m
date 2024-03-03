b438b74dde38dca47d1bd5c0841f93f58a8446af
Rafael Adolfo Blanco Villafañe <blancovillafane@gmail.com>
2024-03-03 17:25:14 +0100

    New auxiliary variables added.

--- config/config.m
+++ config/config.m
@@ -1,14 +1,33 @@
-% Aerodynamic Parameters
+% Geometric Parameters
+R   = 0.165;                        % Propeller radius.
+D   = 2*R;                          % Propeller diameter.
+A   = pi*R*R;                       % Propeller front area.
+L   = 1;                            % Distance from CM to Rotor.
+phi = [45,135,225,315].*(pi/180);   % Rotors angular position.
+
+
+% Propulsion Parameters
+Rbat    = 0.03; % Internal resistence 1 Li-Cell
 a0      = 5.5;
 ct      = 0.012;
 surface = 0.0855;
 CT      = 0.0047;
 CQ      = 0.228e-3;
 Ib      = 40.887e-6;
-R       = 0.165;
-density = 1.184;
+lambda  = 0.00795;  % Torque to current ratio of the motor.
+rho0    = 1.225;    % Air density at see level.
+Cm      = CQ*A*R*R*R*Rbat*rho0/lambda;
+
 omega0  = 870;
 Cd      = 0.01;
+d       = [1,-1,1,-1]; % Rotor directions.
+
+aux         = (D*D*D*D)/(4*pi*pi);
+aux2        = -CQ*D/(CT*L);
+propsMatrix = aux.*[ 1/L,            1/L,            1/L,            1/L;        ...
+                    -sin(phi(1)),   -sin(phi(2)),   -sin(phi(3)),   -sin(phi(4));...
+                    cos(phi(1)),    cos(phi(2)),    cos(phi(3)),    cos(phi(4)); ...
+                    aux2*d(1),      aux2*d(2),      aux2*d(3),      aux2*d(4)];
 
 % Mass Parameters
 mass    = 4.34;
