1fc7e9d9be5caaab57b68f14399eacee6b94c389
Victrobv <ictbasv@gmail.com>
2024-04-09 16:10:43 -0400

    modified config file to use structs simplified drag model

--- config/config.m
+++ config/config.m
@@ -1,71 +1,70 @@
-% Geometric Parameters
-R   = 0.165;                        % Propeller radius.
-D   = 2*R;                          % Propeller diameter.
-A   = pi*R*R;                       % Propeller front area.
-L   = 1;                            % Distance from CM to Rotor.
-phi = [45,135,225,315].*(pi/180);   % Rotors angular position.
+% ======= Model Startup ======= %
+% ============================= %
+
+%------- Physical Parameters -------%
+prop.R    = 0.165;                   % [m]
+prop.D    = 2*prop.R;                % [m]
+prop.Area = pi*prop.R*prop.R;        % [m^2]
+prop.Lcm    = 1;                     % [m]
+prop.phi  = [45,135,225,315].*(pi/180);   % Rotors angular position.
+prop.d    = [1,-1,1,-1];                  % Rotor directions.
+
+%------- Motor Parameters -------%
+motor.Rbat    = 0.03;                     % Internal resistence 1 Li-Cell
+motor.a0      = 5.5;
+motor.ct      = 0.012;
+motor.surf    = 0.0855;
+motor.CT      = 0.0047;
+motor.CQ      = 0.228e-3;
+motor.Ib      = 40.887e-6;
+motor.lambda  = 0.00795;                  % Torque to current ratio of the motor.
+motor.rho0    = 1.225;                    % Air density at sea level.
+motor.Cm      = motor.CQ*prop.Area*prop.R^3*motor.Rbat*motor.rho0/motor.lambda;
+motor.omega0  = 870;
 
 
-% Propulsion Parameters
-Rbat    = 0.03; % Internal resistence 1 Li-Cell
-a0      = 5.5;
-ct      = 0.012;
-surface = 0.0855;
-CT      = 0.0047;
-CQ      = 0.228e-3;
-Ib      = 40.887e-6;
-lambda  = 0.00795;  % Torque to current ratio of the motor.
-rho0    = 1.225;    % Air density at see level.
-Cm      = CQ*A*R*R*R*Rbat*rho0/lambda;
+aux         = (pi*prop.R^2)*(prop.R^2)*(motor.CT*prop.Lcm);
+aux2        = -motor.CQ*prop.R/(motor.CT*prop.Lcm);
 
-omega0  = 870;
-Cd      = 0.01;
-d       = [1,-1,1,-1]; % Rotor directions.
+prop.M      = aux.*[ 1/prop.Lcm,           1/prop.Lcm,          1/prop.Lcm,          1/prop.Lcm;       ...
+                    -sin(prop.phi(1)),    -sin(prop.phi(2)),   -sin(prop.phi(3)),   -sin(prop.phi(4)); ...
+                     cos(prop.phi(1)),     cos(prop.phi(2)),    cos(prop.phi(3)),    cos(prop.phi(4)); ...
+                     aux2*prop.d(1),       aux2*prop.d(2),      aux2*prop.d(3),      aux2*prop.d(4)];
 
-aux         = (pi*R*R)*(R*R)*(CT*L);
-aux2        = -CQ*R/(CT*L);
-propsMatrix = aux.*[ 1/L,            1/L,            1/L,            1/L;        ...
-                    -sin(phi(1)),   -sin(phi(2)),   -sin(phi(3)),   -sin(phi(4));...
-                    cos(phi(1)),    cos(phi(2)),    cos(phi(3)),    cos(phi(4)); ...
-                    aux2*d(1),      aux2*d(2),      aux2*d(3),      aux2*d(4)];
+%------- Inertial Parameters -------%
+uav.mass    = 4.34;             % [kg]
+uav.Ixx     = 0.0820;           % [kg-m^2] 
+uav.Iyy     = 0.0845;           % [kg-m^2]
+uav.Izz     = 0.1377;           % [kg-m^2]
 
-% Mass Parameters
-mass    = 4.34;
-Ixx     = 0.0820;
-Iyy     = 0.0845;
-Izz     = 0.1377;
-I       = [ Ixx 0   0   ;...
-            0   Iyy 0   ;...
-            0   0   Izz];
+uav.I       = [ uav.Ixx 0       0   ;...
+                0       uav.Iyy 0   ;...
+                0       0       uav.Izz];
 
-% Wind conditions
-wx = 0;
-wy = 0;
-wz = 0;
-windSpeedInertial = [wx wy wz];
+uav.Ax = 1;
+uav.Ay = 1;
+uav.Az = 1;
 
-% Initial conditions
-xe = 0;
-ye = 0;
-ze = -2;
-initialPositionInertial = [xe ye ze];
+%------- Wind Conditions -------%
+wind.wx = 0;
+wind.wy = 0;
+wind.wz = 0;
+wind.Speed = [wind.wx wind.wy wind.wz];
 
-u = 0;
-v = 0;
-w = 0;
-initialVelocityBody = [u v w];
+%------- Initial Conditions -------%
+initialState.PositionE = [0 0 -2]; % [pn pe pd]
 
-p = 0;
-q = 0;
-r = 0;
-initialRatesBody = [p q r];
+initialState.VelocityB = [0 0 0];      % [ub vb wb]
 
-roll    = 0;
-pitch   = 0;
-yaw     = 0;
-initialEulerAngles = [roll pitch yaw];
+initialState.RatesB = [0 0 0];         % [p q r]
 
-referenceHeight = 0;
-latitude        = 0;
-longitude       = 0;
-referenceGeodeticPosition = [latitude longitude];
+initialState.EulerB = [0 0 0];       % [roll pitch yaw]
+
+%------- References -------%
+reference.H = 0;
+
+reference.latitude = 0;
+reference.longitude = 0;
+
+reference.G = [reference.latitude ...
+               reference.longitude];
